9e3df6d734b59be1f860d3ccc2e1b09bd0001068
DenisOuma <dennisouma38@gmail.com>
2022-09-01 19:36:54 +0300

    Working on the dialog section

--- js/detail.js
+++ js/detail.js
@@ -1,105 +1,13 @@
-// fetch("https://reqres.in/api/users")
-// 	.then((res) => {
-// 		if (res.ok) {
-// 			console.log("Your Fetch request was successful");
-// 		} else {
-// 			console.log("Your Fetch request failed");
-// 		}
-// 		return res.json();
-// 	})
-// 	.then((users) => handleData(users))
-// 	.catch((err) => console.log("The addres enterd is not valid"));
+function getCardDetails() {
+	const ship = document.querySelector(".view-more");
+	const cardDiv = document.querySelector(".product");
+	cardDiv.style.display = "block";
+}
 
-// function handeStarWarsShipData() {
-// 	console.log("Hello World");
-// }
-// async function fetchProductDetail() {
-// 	const urlParams = new URLSearchParams(window.location.search);
-// 	const myParam = urlParams.get("url");
-// 	console.log(myParam);
-// 	const res = await fetch(myParam);
-// 	const jsonData = await res.json();
-// 	console.log(jsonData);
-// 	const ship = jsonData;
-// 	console.log(ship);
-document.getElementById("product-div").innerHTML = `
-    <div class="product p-4">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex align-items-center"><a href="home.html"><i class="fas fa-long-arrow-alt-left"></i> <span class="ml-1">Back</span></a></div>
-                                <i class="fas fa-shopping-cart text-muted"></i>
-                            </div>
-                            <div class="mt-4 mb-3"> <span class="text-uppercase text-muted brand">STARWAR SHIP</span>
-                                <h5 class="text-uppercase"> shipName:${
-																	ship.name
-																}</h5>
-                                <div class="price d-flex flex-row align-items-center"> <span class="act-price">$${
-																	ship.cost_in_credits
-																}</span>
-                        </div>
-                    </div>
-                    <p class="about">model:  ${ship.model}</p>
-                    <p class="about">manufacturer: ${ship.manufacturer}</p>
-                    <p class="about">length: ${ship.length}</p>
-                    <p class="about">max_atmosphering_speed: ${
-											ship.max_atmosphering_speed
-										}</p>
-                    <p class="about">crew: ${ship.crew}</p>
-                    <p class="about">passengers: ${ship.passengers}</p>
-                    <p class="about">consumables: ${ship.consumables}</p>
-                    <p class="about">hyperdrive_rating: ${
-											ship.hyperdrive_rating
-										}</p>
-                    <p class="about">MGLT: ${ship.MGLT}</p>
-                    <p class="about">starship_class ${ship.length}</p>
-                    <p class="about">pilots: ${
-											ship.pilots.length > 0 ? ship.pilots[0] : "none"
-										}</p>
-                    <p class="about">films: ${ship.films[0]}</p>
-                    <p class="about">created: ${ship.created}</p>
-                    <p class="about">edited: ${ship.edited}</p>
-                    <p class="about">Link: ${ship.url}</p>
-                </div>
-                <div class="cart mt-4 align-items-center"> <button class="btn btn-danger  text-uppercase mr-2 px-4" id="cart" >Add to cart</button>
-    <i class="fa fa-heart text-muted"></i> <i class="fa fa-share-alt text-muted"></i> </div>
-    `;
+document.addEventListener("DOMContentLoaded", () => {
+	getShipName = document.getElementsByClassName("card");
+	// handleData();
 
-// document.getElementById("cart").addEventListener("click", function (e) {
-// 	if (localStorage.getItem("cart") !== null) {
-// 		let cart = JSON.parse(localStorage.getItem("cart"));
-// 		cart.cartitems.push({
-// 			name: ship.name,
-// 			length: ship.length,
-// 			manufacturer: ship.manufacturer,
-// 			cost: ship.cost_in_credits,
-// 		});
-// 		localStorage.setItem("cart", JSON.stringify(cart));
-// 		console.log(cart.cartitems);
-// 		let total = cart.cartitems.reduce((a, c) => {
-// 			return a + Number(c.cost);
-// 		}, 0);
-// 		document.getElementById("cart-total").innerText = total;
-// 	} else {
-// 		let items = {
-// 			cartitems: [
-// 				{
-// 					name: ship.name,
-// 					length: ship.length,
-// 					manufacturer: ship.manufacturer,
-// 					cost: ship.cost_in_credits,
-// 				},
-// 			],
-// 		};
-
-// 		localStorage.setItem("cart", JSON.stringify(items));
-// 	}
-// });
-// }
-// function dialogPage() {
-// 	console.log("Hello World");
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-// 	// users = document.getElementById("users");
-// 	// handleData();
-// 	// createUser();
-// });
+	// getCardDetails();
+	// createUser();
+});
--- js/home.js
+++ js/home.js
@@ -20,14 +20,54 @@ async function getShip(url) {
                     <div class="card">
                         <img src="images/starwar.jpg" alt="" class="card-img-top">
                         <div class="card-body">
-                        <h5>${ship.name}</h5>
+                        <h5 id = "ship-name">${ship.name}</h5>
                         <h5>$ ${ship.cost_in_credits}</h5>
                         <h5>length: ${ship.length}</h5>
 						<h5>length: ${ship.length}</h5>
-                        <button id="details" type="button" onClick="dialogPage()" class="btn btn-success" ><i class="fas fa-cart-plus"></i>Detail</button>
+                        <button type="button" onClick="getCardDetails()" class="view-more btn btn-success" ><i class="fas fa-cart-plus"></i>Detail</button>
+                        </div>
+						<div class="product ">
+	                          <div class="d-flex justify-content-between align-items-center">
+	                              <div class="d-flex align-items-center"><a href="home.html"><i class="fas fa-long-arrow-alt-left"></i> <span class="ml-1">Back</span></a></div>
+	                              <i class="fas fa-shopping-cart text-muted"></i>
+	                          </div>
+	                          <div class="mt-4 mb-3"> <span class="text-uppercase text-muted brand">STARWAR SHIP</span>
+	                              <h5 class="text-uppercase"> shipName:${
+																	ship.name
+																}</h5>
+	                              <div class="price d-flex flex-row align-items-center"> <span class="act-price">$${
+																	ship.cost_in_credits
+																}</span>
+	                      </div>
+	                  </div>
+	                  <p class="about">model:  ${ship.model}</p>
+	                  <p class="about">manufacturer: ${ship.manufacturer}</p>
+	                  <p class="about">length: ${ship.length}</p>
+	                  <p class="about">max_atmosphering_speed: ${
+											ship.max_atmosphering_speed
+										}</p>
+	                  <p class="about">crew: ${ship.crew}</p>
+	                  <p class="about">passengers: ${ship.passengers}</p>
+	                  <p class="about">consumables: ${ship.consumables}</p>
+	                  <p class="about">hyperdrive_rating: ${
+											ship.hyperdrive_rating
+										}</p>
+	                  <p class="about">MGLT: ${ship.MGLT}</p>
+	                  <p class="about">starship_class ${ship.length}</p>
+	                  <p class="about">pilots: ${
+											ship.pilots.length > 0 ? ship.pilots[0] : "none"
+										}</p>
+	                  <p class="about">films: ${ship.films[0]}</p>
+	                  <p class="about">created: ${ship.created}</p>
+	                  <p class="about">edited: ${ship.edited}</p>
+	                  <p class="about">Link: ${ship.url}</p>
+	              </div>
+	              <div class="cart mt-4 align-items-center"> <button class="btn btn-danger  text-uppercase mr-2 px-4" id="cart" >Add to cart</button>
+	  <i class="fa fa-heart text-muted"></i> <i class="fa fa-share-alt text-muted"></i> </div>
                         </div>
                         </div>
-                        </div>
+						
+						
                 `;
 		});
 		cardDiv.innerHTML = list;
